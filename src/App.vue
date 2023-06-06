4ea5562a50d867f10a85a3244847eed0796dffd5
FRAALES <alessandrofranco1992@gmail.com>
2023-06-06 16:08:55 +0200

    charactersList setuppato

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue'
+import CharactersList from './components/CharactersList.vue'
+
+
+export default {
+  components: {
+    AppHeader,
+    CharactersList,
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader message="Yugi-oh Api" />
+  <CharactersList />
 </template>
 
 <style lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+@use './styles/general' as *;
+@use './styles/partials/variables' as *;
 </style>
---
+++ src/components/CharactersList.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: "CharactersList",
+}
+
+
+</script>
+
+
+<template>
+    <section class="container">
+
+
+
+        <!-- card character di esempio -->
+        <div class="character text-center">
+            <img src="info.image" alt="info.name" class="img-fluid mb-3">
+            <h4>name</h4>
+            <div class="font-weight-bold">archetype</div>
+        </div>
+
+
+
+
+
+
+    </section>
+</template>
+
+
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+
+h4 {
+    text-transform: capitalize;
+
+    &::after {
+        content: '';
+        display: block;
+        width: 40px;
+        height: 1px;
+        margin: 1rem auto;
+    }
+}
+
+img {
+    box-shadow: 0 0 6px 3px rgba(#000, 0.15);
+}
+</style>
